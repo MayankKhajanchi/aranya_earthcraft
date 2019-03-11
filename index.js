97df6d81bd03ef5fdb2fb19f5a1848ad1f285015
MayankKhajanchi <mayankkhajnchi@gmal.com>
2019-03-11 13:33:00 +0530

    nav dots added and page furbished

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // console.log("heyo");
-$(window).bind('mousewheel', function(event) {
-if (event.originalEvent.wheelDeltaY >= 200) {
-    console.log('Scroll up');
-}
-else {
-    console.log('Scroll down');
-}
-});
+
+// $(window).bind('mousewheel', function(event) {
+// if (event.originalEvent.wheelDeltaY >= 200) {
+//     console.log('Scroll up');
+// }
+// else {
+//     console.log('Scroll down');
+// }
+// });
